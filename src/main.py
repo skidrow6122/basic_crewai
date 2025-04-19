27fbba171cff660bc8dd7270e4505b02409cff89
jeonhyeongjun <skidrow6122@gmail.com>
2025-04-19 11:14:37 +0900

    Initial commit

---
+++ src/main.py
@@ -0,0 +1,16 @@
+from crewai import Agent, Task, Crew
+from langchain_openai import ChatOpenAI
+import os
+
+os.environ['OPENAI_API_KEY'] = 'xxxxxxx'
+
+
+# 목차 설정 에이전트
+outline_generator = Agent(
+    role="Outline Generator",
+    goal="Create structured outlines for articles on given topics. answer in Korean",
+    llm=ChatOpenAI(model= "gpt-4o-mini", max_tokens = 1000),
+    backstory="You are an expert at organizing information and creating comprehensive outlines for various subjects."
+)
+
+print("SS")
